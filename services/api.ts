8f1f1300898abfdf30fd94b23e3b8258094efb5c
Anderson <anderson.consultant.it@outlook.com>
2022-03-06 14:34:57 -0500

    feat: axios refresh token strategy to avoid making requests with old token while we are getting a new one
    whenever a 401 happens and it is a token expired error, if it is the first time (!isRefreshing) then we request a refresh token and update the cookies.
    
    For all requests that were attempted meanwhile, they are added to a queue with the configs used.
    
    At the end of the refresh token update process, we retry all the failed requests with their original configuration but using the new token

--- services/api.ts
+++ services/api.ts
@@ -2,6 +2,8 @@ import axios, { AxiosError } from 'axios';
 import { parseCookies, setCookie } from 'nookies';
 
 let cookies = parseCookies();
+let isRefreshing = false;
+let failedRequestsQueue: any[] = [];
 
 export const api = axios.create({
   baseURL: 'http://localhost:3333',
@@ -21,30 +23,70 @@ api.interceptors.response.use((response) => response, (error: AxiosError) => {
       // Extracts the refresh token from the cookies
       const { '<my_app_name_refresh_token>': refreshToken } = cookies;
 
-      api.post('/refresh', {
-        refreshToken,
-      }).then((response) => {
-        const { token } = response.data;
+      // Configuration used in the request that failed
+      const originalConfig = error.config;
 
-        // Save JWT token in cookie
-        setCookie(undefined, '<my_app_name_token>', token, {
-          maxAge: 60 * 60 * 24 * 30, // 30 days,
-          path: '/',
+      // First time, it tries to refresh the token
+      if (!isRefreshing) {
+        // Sets the flag to true to avoid multiple simultaneous refresh requests
+        isRefreshing = true;
+
+        api.post('/refresh', {
+          refreshToken,
+        }).then((response) => {
+          const { token } = response.data;
+
+          // Save JWT token in cookie
+          setCookie(undefined, '<my_app_name_token>', token, {
+            maxAge: 60 * 60 * 24 * 30, // 30 days,
+            path: '/',
+          });
+
+          // Save refresh token in cookie
+          setCookie(undefined, '<my_app_name_refresh_token>', response.data.refreshToken, {
+            maxAge: 60 * 60 * 24 * 30, // 30 days,
+            path: '/',
+          });
+
+          // Since our default token being sent to all requests changed because it had expired,
+          // we need to update the default headers with the new token
+          // Obs: it has to be default.headers.common and not .defaults.headers on new axios version
+          api.defaults.headers.common['Authorization'] = `Bearer ${token}`;
+
+          // Retry the requests that failed with new token
+          failedRequestsQueue.forEach((request) => request.onSuccess(token));
+          failedRequestsQueue = [];
+        }).catch((err) => {
+          // Retry the requests that failed with the same config
+          failedRequestsQueue.forEach((request) => request.onFailure(err));
+          console.error(error);
+        }).finally(() => {
+          // Sets the flag to false to allow the next refresh request
+          isRefreshing = false;
         });
+      }
 
-        // Save refresh token in cookie
-        setCookie(undefined, '<my_app_name_refresh_token>', response.data.refreshToken, {
-          maxAge: 60 * 60 * 24 * 30, // 30 days,
-          path: '/',
+      // We are returning a new promise because axios interceptors do not accept async await
+      return new Promise((resolve, reject) => {
+        failedRequestsQueue.push({
+          // Refresh token worked so retry the failed request with new token
+          onSuccess: (token: string) => {
+            // api.defaults.headers.common['Authorization'] = `Bearer ${token}`;
+
+            // Added ! to tell typescript that the headers are not null
+            originalConfig.headers!['Authorization'] = `Bearer ${token}`;
+
+            resolve(api(originalConfig));
+          },
+
+          // Refresh token didn't work so reject the request. Not much we can do at this point.
+          onFailure: (err: AxiosError) => {
+            reject(err);
+          },
+          error,
         });
-
-        // Since our default token being sent to all requests changed because it had expired,
-        // we need to update the default headers with the new token
-        // Obs: it has to be default.headers.common and not .defaults.headers on new axios version
-        api.defaults.headers.common['Authorization'] = `Bearer ${token}`;
-      }).catch((error) => {});
-    } else {
-      // log out the user
+      });
     }
+    // log out the user
   }
 });
