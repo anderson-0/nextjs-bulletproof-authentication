81915396eb87bba881ca5438722bca26554a5123
Anderson <anderson.consultant.it@outlook.com>
2022-03-06 11:27:45 -0500

    refactor: renamed context folder to contexts

---
+++ contexts/AuthContext.tsx
@@ -0,0 +1,57 @@
+import { useRouter } from 'next/router';
+import { createContext, useState } from 'react';
+import { api } from '../services/api';
+
+type User = {
+  email: string;
+  permissions: string[];
+  roles: string[];
+}
+
+type SignInCredentials = {
+  email: string;
+  password: string;
+};
+
+type AuthContextData = {
+  signIn(credentials: SignInCredentials): Promise<void>
+  user?: User;
+  isAuthenticated: boolean;
+}
+
+type AuthProviderProps = {
+  children: React.ReactNode;
+}
+
+export function AuthProvider({ children }: AuthProviderProps) {
+  const [user, setUser] = useState<User>();
+  const router = useRouter();
+  const isAuthenticated = !!user;
+
+  async function signIn({ email, password }: SignInCredentials) {
+    console.log(email, password);
+    try {
+      const response = await api.post('sessions', {
+        email,
+        password,
+      });
+      console.log(response.data)
+      
+      const { permissions, roles } = response.data;
+      setUser({ email, permissions, roles });
+
+      router.push('/dashboard');
+
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  return (
+    <AuthContext.Provider value={{ signIn, isAuthenticated, user}}>
+      {children}
+    </AuthContext.Provider>
+  )
+}
+
+export const AuthContext = createContext({} as AuthContextData);
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,21 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
-import { FormEvent, useState } from 'react'
+import { FormEvent, useContext, useState } from 'react'
+import { AuthContext } from '../contexts/AuthContext'
 import styles from '../styles/Home.module.css'
 
 const Home: NextPage = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
-  function handleSubmit(e: FormEvent<HTMLFormElement>) {
+  const { signIn } = useContext(AuthContext)
+
+  async function handleSubmit(e: FormEvent<HTMLFormElement>) {
     e.preventDefault();
     console.log(email, password);
+
+    await signIn({ email, password })
   }
 
   return (
