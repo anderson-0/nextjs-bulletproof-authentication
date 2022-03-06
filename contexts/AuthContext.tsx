15484fb52d79899303bf6874fa6d878874d6cf88
Anderson <anderson.consultant.it@outlook.com>
2022-03-06 13:20:39 -0500

    feat: saving token and refresh token inside context and sending token with every request

--- contexts/AuthContext.tsx
+++ contexts/AuthContext.tsx
@@ -1,5 +1,6 @@
 import { useRouter } from 'next/router';
-import { createContext, useState } from 'react';
+import { setCookie, parseCookies } from 'nookies';
+import { createContext, useEffect, useState } from 'react';
 import { api } from '../services/api';
 
 type User = {
@@ -22,11 +23,22 @@ type AuthContextData = {
 type AuthProviderProps = {
   children: React.ReactNode;
 }
+export const AuthContext = createContext({} as AuthContextData);
 
-export function AuthProvider({ children }: AuthProviderProps) {
+export const AuthProvider = ({ children }: AuthProviderProps) => {
   const [user, setUser] = useState<User>();
   const router = useRouter();
   const isAuthenticated = !!user;
+
+  useEffect(() => {
+    const { '<my_app_name_token>': token, '<my_app_name_refresh_token>': refreshToken } = parseCookies();
+    if (token) {
+      api.get('/me').then((response) => {
+        const { email, permissions, roles } = response.data;
+        setUser({ email, permissions, roles });
+      });
+    }
+  }, []);
 
   async function signIn({ email, password }: SignInCredentials) {
     console.log(email, password);
@@ -36,13 +48,29 @@ export function AuthProvider({ children }: AuthProviderProps) {
         password,
       });
 
-      const { permissions, roles } = response.data;
+      const {
+        token, refreshToken, permissions, roles,
+      } = response.data;
+
+      // Save JWT token in cookie
+      setCookie(undefined, '<my_app_name_token>', token, {
+        maxAge: 60 * 60 * 24 * 30, // 30 days,
+        path: '/',
+      });
+
+      // Save refresh token in cookie
+      setCookie(undefined, '<my_app_name_refresh_token>', refreshToken, {
+        maxAge: 60 * 60 * 24 * 30, // 30 days,
+        path: '/',
+      });
 
       setUser({
         email,
         permissions,
         roles,
       });
+
+      api.defaults.headers.common['Authorization'] = `Bearer ${token}`;
 
       router.push('/dashboard');
     } catch (error) {
@@ -55,6 +83,4 @@ export function AuthProvider({ children }: AuthProviderProps) {
       {children}
     </AuthContext.Provider>
   );
-}
-
-export const AuthContext = createContext({} as AuthContextData);
+};
